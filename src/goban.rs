d47d258798dfd9f4cc27920383fc26510b211e75
saitotm <tomokazu.ml.tech@gmail.com>
2022-05-21 12:09:33 +0900

    fix to use the current shell to launch a command

--- src/goban.rs
+++ src/goban.rs
@@ -1,11 +1,12 @@
 use std::{
     collections::HashMap,
+    env,
     io::{self, Write},
     process::{Command, Output},
 };
 
-use std::hash::Hash;
 use std::fs;
+use std::hash::Hash;
 
 use serde_json::Value;
 
@@ -35,27 +36,32 @@ impl<T: Translator> Goban<T> {
 
         for (i, param) in params.iter().enumerate() {
             let cmd = self.translator.render(&param, &self.command);
+            let shell = self.get_current_shell().unwrap_or_else(|| "sh".to_string());
 
             println!("\n[{} / {}]", i + 1, params.get_combination());
             println!("Parameters: {:?}", &param); // FIXME: show keys in the same order everytime.
             println!("{}", &cmd);
 
-            let output = self.run_command(cmd);
+            let output = self.run_command(shell, cmd);
 
             // FIXME: remove unwrap
-            io::stdout().write_all(&output.stdout).unwrap(); 
+            io::stdout().write_all(&output.stdout).unwrap();
             io::stderr().write_all(&output.stderr).unwrap();
             println!("[{}]", output.status);
         }
     }
 
-    fn run_command(&self, cmd: String) -> Output {
-        // FIXME: execute command on the current shell.
-        Command::new("sh")
-                .arg("-c")
-                .arg(cmd)
-                .output()
-                .expect("failed to execute process")
+    fn run_command(&self, shell: String, cmd: String) -> Output {
+        Command::new(shell)
+            .arg("-c")
+            .arg(cmd)
+            .output()
+            .expect("failed to execute process")
+    }
+
+    // FIXME: Return Result instead of Option
+    fn get_current_shell(&self) -> Option<String> {
+        env::var("SHELL").ok()
     }
 
     fn read_file(&self) -> String {
@@ -65,7 +71,10 @@ impl<T: Translator> Goban<T> {
 
     fn split_keys_values<K: Hash + Eq + Clone, V: Clone>(kvm: HashMap<K, V>) -> (Vec<K>, Vec<V>) {
         let keys = kvm.keys().cloned().collect::<Vec<K>>();
-        let values = keys.iter().map(|k| kvm.get(k).expect("it must be Some").clone()).collect::<Vec<V>>();
+        let values = keys
+            .iter()
+            .map(|k| kvm.get(k).expect("it must be Some").clone())
+            .collect::<Vec<V>>();
 
         (keys, values)
     }
